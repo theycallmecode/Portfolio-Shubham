1f7a976e1bc1e8c1b097e7c45d2ecfb4d7f1d34b
Shubham Raj <ershubhcsofficial@gmail.com>
2026-01-18 02:59:46 +0530

    defined colors in index.css

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+export const Home = () => {
+    return (
+        <div className="min-h-screen ">
+
+            { /* Theme Toggle Button */ }
+
+            { /* Background Effects */ }
+
+            { /* Navbar */ }
+
+            { /* Main Content */ }
+
+            { /* Footer */ }
+        </div>
+    );
+};
